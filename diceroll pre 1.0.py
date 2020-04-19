72936b736fb6d897ec4fae40b1744646bd052194
CamKoepele <61732982+CamKoepele@users.noreply.github.com>
2020-04-20 10:09:25 +1200

    pre 1.1
    Added percentage calculations. Sectioned into funcs.
    Program now outputs as required

--- diceroll pre 1.0.py
+++ diceroll pre 1.0.py
@@ -5,17 +5,22 @@
 import random
 
 #create func to roll to dice and find sum
-def roll:
+def roll():
     total = random.randint(1,6) + random.randint(1,6)
     return total
-total = {2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0, 10:0, 11:0, 12:0}
-expected_outcome = {2:2.78, 3:5.56, 4:8.33, 5:11.11, 6:13.89, 7:16.67, 8:13.89, 9:11.11, 10:8.33, 11:5.56, 12:2.78}
-percent =
+def main():
+    total = {2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0, 10:0, 11:0, 12:0}
+    expected_outcome = {2:2.78, 3:5.56, 4:8.33, 5:11.11, 6:13.89, 7:16.67, 8:13.89, 9:11.11, 10:8.33, 11:5.56, 12:2.78}
+    percent = {}
 
-#roll 1k
-for i in range (1000):
-    outcome = roll()
-    total[outcome] += 1
+    #roll 1k
+    for i in range (1000):
+        outcome = roll()
+        total[outcome] += 1
 
-    
-    
+    #calculate %
+    for i in range(2,13):
+        total[i] = total[i]/10
+        print("Total:{} \tPercentage:{}  Expected Percentage:{}".format(i, total[i], expected_outcome[i]))
+        
+main()    
